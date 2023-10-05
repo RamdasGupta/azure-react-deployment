c5ce2cadaad6ad0cc4a5303775258a3080dd611a
Ramdas Gupta <er.rdgupta@gmail.com>
2023-10-05 06:55:36 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch(apiBaseURL + "/task").then((data) => {
+      return data.json()
+    }).then((data) => {
+      console.log("Data", data);
+      setTasks(data.data)
+    }).catch((error) => {
+      console.log("error", error);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Welcome to the Task Manager</h1>
+      {tasks && tasks.map((task, index) => {
+        return (<h3 key={index}>{task.title}</h3>)
+      })}
     </div>
   );
 }
